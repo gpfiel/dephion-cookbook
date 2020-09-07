dff46f1508d006b1c9ba1cf45bf844320a4acc97
gpfiel <gpfiel@hotmail.com>
2020-09-07 12:34:14 +0200

    Added recipes ID generator

--- app/routes/recipes.js
+++ app/routes/recipes.js
@@ -3,13 +3,20 @@ import { A } from '@ember/array';
 
 export default class RecipesRoute extends Route {
 
+  guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return (s4() + '-' + s4()).toUpperCase()
+	}
+
   setupController(controller, model) {
     super.setupController(controller, model)
     let arrRecipes = A([])
     for (let index = 1; index <= 10; index++) {
       arrRecipes.pushObject(
         this.store.createRecord('recipe', {
-          id: index,
+          id: this.guid(),
           name: `Recipe ${index}`,
           description: `Description ${index}`,
           numberServings: Math.round(Math.random()),
